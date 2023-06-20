a5df4fa2a644b7f1f5f0f8b737f0401fbf455b22
yangyangdi <1@1.cn>
2023-06-20 15:52:47 +0800

    feature(routes/+page.svelte): add main page layout and page

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -6,7 +6,20 @@
 	// Most of your app wide CSS should be put in this file
 	import '../app.postcss';
 	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import ColorSwitch from '../components/ColorSwitch.svelte';
+
+	import hljs from 'highlight.js';
+	import 'highlight.js/styles/github-dark.css';
+	import { storeHighlightJs } from '@skeletonlabs/skeleton';
+
+	storeHighlightJs.set(hljs);
+
+	let appName = 'Shiro Pad';
 </script>
+
+<svelte:head>
+	<title>{appName}</title>
+</svelte:head>
 
 <!-- App Shell -->
 <AppShell>
@@ -14,33 +27,12 @@
 		<!-- App Bar -->
 		<AppBar>
 			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Skeleton</strong>
+				<a href="/">
+					<strong class="text-xl uppercase">{appName}</strong>
+				</a>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
-				</a>
+				<ColorSwitch />
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
---
+++ frontend/src/routes/pad/[padId]/+page.svelte
@@ -0,0 +1,54 @@
+<script>
+	import { CodeBlock } from '@skeletonlabs/skeleton';
+	import { clipboard } from '@skeletonlabs/skeleton';
+
+	import BackIcon from '../../../assets/icons/back.svelte';
+	import CopyIcon from '../../../assets/icons/copy.svelte';
+	import CopiedIcon from '../../../assets/icons/check.svelte';
+
+	let code = '<div>This is meta</div>';
+	let copied = false;
+</script>
+
+<div class="flex h-full">
+	<div class="card w-1/2 p-2 m-2">
+		<a href="/" class="btn btn-sm variant-filled-surface">
+			<span><BackIcon /></span>
+			返回
+		</a>
+		<label class="label my-4 h-3/4 flex flex-col">
+			<span>粘贴代码</span>
+			<textarea
+				class="w-full flex-grow dark:bg-slate-500 font-mono p-2 rounded-md"
+				placeholder="粘贴代码至这里..."
+				bind:value={code}
+			/>
+		</label>
+		<label class="label">
+			<span>语言</span>
+			<select class="select">
+				<option value="1">JavaScript</option>
+				<option value="2">Option 2</option>
+				<option value="3">Option 3</option>
+				<option value="4">Option 4</option>
+				<option value="5">Option 5</option>
+			</select>
+		</label>
+		<div>
+			<button
+				class="btn btn-sm variant-filled"
+				use:clipboard={window.location.href}
+				on:click={() => (copied = true)}
+			>
+				{#if copied}
+					<span><CopiedIcon /></span>已复制
+				{:else}
+					<span><CopyIcon /></span>复制地址
+				{/if}
+			</button>
+		</div>
+	</div>
+	<div class="card w-1/2 p-2 m-2">
+		<CodeBlock class="w-full h-full" language="html" buttonLabel="复制" code={`${code}`} />
+	</div>
+</div>
