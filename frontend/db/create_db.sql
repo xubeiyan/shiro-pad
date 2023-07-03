9ff0e559a700c1757ca66ca5e3218925e23c3a4e
yangyangdi <1@1.cn>
2023-07-03 10:36:52 +0800

    add create_db.sql, update git ignore

---
+++ frontend/db/create_db.sql
@@ -0,0 +1,11 @@
+CREATE TABLE "pad" (
+	"id"	INTEGER NOT NULL,
+	"ulid"	TEXT NOT NULL,
+	"language"	TEXT NOT NULL,
+	"keepTime"	TEXT NOT NULL,
+	"createAt"	INTEGER NOT NULL,
+	"expireAt"	INTEGER NOT NULL,
+	"expired"	INTEGER NOT NULL DEFAULT 0,
+	"codeText"	TEXT,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
