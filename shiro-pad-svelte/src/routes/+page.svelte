d406b7956f6e42529a756b08c2ef04c85c6be899
yangyangdi <1@1.cn>
2023-07-07 10:27:49 +0800

    main folder rename to `shiro-pad-svelte`

---
+++ shiro-pad-svelte/src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-10 text-center flex flex-col items-center">
+		<h2 class="h2">Shiro Pad, 一个白板应用</h2>
+		<div class="flex justify-center space-x-2">
+			<a
+				class="btn variant-filled"
+				href="/pad/new"
+			>
+				粘点东西
+			</a>
+			<a
+				class="btn variant-outline"
+				href="/pad/1"
+			>
+				看看例子
+			</a>
+		</div>
+	</div>
+</div>
+
+<style lang="postcss">
+	
+</style>
